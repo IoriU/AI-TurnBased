763f69f27ff42b8ff1b93fa36a4fc4aae999e32d
IvanM63 <ivanmaulana.agusti@gmail.com>
2023-07-16 00:56:43 +0700

    fix selectTarget

--- Assets/Prefab/Controller/UiController.cs
+++ Assets/Prefab/Controller/UiController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 
 public class UiController : MonoBehaviour
 {
@@ -12,15 +13,18 @@ public class UiController : MonoBehaviour
 
     public void SetSkillButtons(Skill[] skills)
     {
+        this.skills = skills;
         for (int i = 0; i < skills.Length; i++)
         {
             Skill skill = skills[i];
             texts[i].text = skill.name;
+            
         }
     }
 
     public void OnButtonClick(int n)
     {
         watcher.SetSkill(skills[n]);
+        
     }
 }
